85714086db1ede76bc43a0739997409b1a69638f
supriya sahoo <supriyasahoo1011@gmail.com>
2022-12-17 18:25:40 +0530

    file added

--- my-lyst/src/Routes/MainRoutes.jsx
+++ my-lyst/src/Routes/MainRoutes.jsx
@@ -3,10 +3,12 @@ import { Route, Routes } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import Home from "../pages/Home";
 import Products from "../pages/Products";
+import Cart from "../pages/Cart";
 import SignUp from "../pages/sign_up/SignUp";
 import Login from "../pages/log_in/Login";
 import LoggedIn from "../pages/logged-in/LoggedIn";
-
+import { About_us } from "../pages/Help Center/About_us";
+import Footer from "../components/Footer";
 const MainRoutes = () => {
   return (
     <>
@@ -14,10 +16,13 @@ const MainRoutes = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/products/:id" element={<Products />} />
+        <Route path="/cart" element={<Cart />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/login" element={<Login />} />
         <Route path="/logged-in" element={<LoggedIn />} />
+        <Route path="/aboutus" element={<About_us />} />
       </Routes>
+      <Footer />
     </>
   );
 };
